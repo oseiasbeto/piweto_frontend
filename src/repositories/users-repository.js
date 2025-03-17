8c8daeaedbe3eaf6da4ed9b5a34c2e70038008cf
Oséias BC <oseiasbetodev@gmail.com>
2025-03-17 22:54:30 +0100

    Melhorei o fluxo de autenticacao

--- src/repositories/users-repository.js
+++ src/repositories/users-repository.js
@@ -1,215 +1,227 @@
-import api from "../api";
-import { ref } from 'vue';
-import { useStore } from "vuex";
-import Cookies from "js-cookie";
-import { jwtDecode } from "jwt-decode";
+// Importa a instância da API configurada para fazer requisições ao backend.
+import api from "../api"; 
 
-export function useUsers() {
-    const loading = ref(false);
-    const error = ref(false);
-    const store = useStore()
+// Importa `ref` do Vue para criar variáveis reativas.
+import { ref } from 'vue'; 
 
-    let refreshTimeout = null;
+// Importa `useStore` do Vuex para acessar o estado global da aplicação.
+import { useStore } from "vuex"; 
 
-    const register = async (data) => {
+// Importa a biblioteca js-cookie para manipulação de cookies.
+import Cookies from "js-cookie"; 
+
+// Importa a função `jwtDecode` para decodificar tokens JWT.
+import { jwtDecode } from "jwt-decode"; 
+
+// Exporta a função `useUsers`, que retorna métodos relacionados à autenticação e gerenciamento de usuários.
+export function useUsers() { 
+    // Variável reativa para indicar carregamento de alguma operação assíncrona.
+    const loading = ref(false); 
+    
+    // Variável reativa para armazenar possíveis erros.
+    const error = ref(false); 
+    
+    // Obtém a instância do Vuex Store.
+    const store = useStore(); 
+
+    // Variável para armazenar o identificador do timeout de renovação do token.
+    let refreshTimeout = null; 
+
+    // Função assíncrona para registrar um novo usuário.
+    const register = async (data) => { 
         try {
-            loading.value = true
-            await api.post("/users/register", data)
+            loading.value = true; // Ativa o estado de carregamento.
+            await api.post("/users/register", data); // Envia os dados do usuário para o backend.
 
-            store.dispatch("setUser", data)
+            store.dispatch("setUser", data); // Atualiza o estado global do usuário.
         } catch (err) {
-            console.log(err.message)
-            throw err
+            console.log(err.message); // Exibe o erro no console.
+            throw err; // Relança o erro.
         } finally {
-            loading.value = false;
+            loading.value = false; // Desativa o estado de carregamento.
         }
-    }
+    };
 
-
-
-    // tem como finalidade refrescar o token de acesso
+    // Função para renovar o token de acesso.
     const refreshToken = async (session_id) => {
         try {
-            loading.value = true
-            await api.post("/users/refresh-access-token", {
-                session_id
-            }).then(res => {
-                const user = res.data.user
-                const accessToken = res.data.access_token
-                store.dispatch("setUser", user)
-                store.dispatch("setAccessToken", accessToken)
-                scheduleTokenRefresh(accessToken)
-            }).finally(() => {
-                loading.value = false
-            })
+            loading.value = true;
+            await api.post("/users/refresh-access-token", { session_id })
+                .then(res => {
+                    const user = res.data.user;
+                    const accessToken = res.data.access_token;
+                    
+                    store.dispatch("setUser", user); // Atualiza o usuário no estado global.
+                    store.dispatch("setAccessToken", accessToken); // Atualiza o token no estado global.
+                    
+                   // scheduleTokenRefresh(accessToken); // Agenda a próxima renovação do token.
+                })
+                .finally(() => {
+                    loading.value = false;
+                });
         } catch (err) {
-            console.log(err)
-            clearRefreshToken()
-            console.log(err.message)
-            throw err
+            console.log(err);
+            clearRefreshToken(); // Limpa o token armazenado.
+            console.log(err.message);
+            throw err;
         }
-    }
+    };
 
-    // tem como finalidade autenticar o usuario no sistema.
+    // Função para autenticar o usuário.
     const auth = async (data) => {
         try {
-            loading.value = true
+            loading.value = true;
 
-            const response = await api.post("/users/auth", data)
-            const user = response.data.user
-            const accessToken = response.data.access_token
-            const sessionId = response.data.session_id
+            const response = await api.post("/users/auth", data);
+            const user = response.data.user;
+            const accessToken = response.data.access_token;
+            const sessionId = response.data.session_id;
 
-            store.dispatch("setUser", user)
-            store.dispatch("setAccessToken", accessToken)
+            store.dispatch("setUser", user);
+            store.dispatch("setAccessToken", accessToken);
 
-            scheduleTokenRefresh(accessToken)
-            setRefreshTokenFromCookies(sessionId)
+            //scheduleTokenRefresh(accessToken);
+            setRefreshTokenFromCookies(sessionId);
 
-            return response
+            return response;
         } catch (err) {
-            console.log(err.message)
-            throw err
+            console.log(err.message);
+            throw err;
         } finally {
             loading.value = false;
         }
-    }
-    // tem como finalidade autenticar o usuario no sistema.
+    };
+
+    // Função para verificar se um e-mail já está cadastrado.
     const checkEmail = async (email) => {
         try {
-            loading.value = true
-
-            const response = await api.put("/users/check-email", {
-                email
-            })
-            return response
+            loading.value = true;
+            return await api.put("/users/check-email", { email });
         } catch (err) {
-            console.log(err.message)
-            throw err
+            console.log(err.message);
+            throw err;
         } finally {
             loading.value = false;
         }
-    }
+    };
+
+    // Função para ativar um e-mail com um token.
     const activeEmail = async (token) => {
         try {
-            loading.value = true
-
-            const response = await api.put("/users/active-email", {
-                token
-            })
-            return response
+            loading.value = true;
+            return await api.put("/users/active-email", { token });
         } catch (err) {
-            console.log(err.message)
-            throw err
+            console.log(err.message);
+            throw err;
         } finally {
             loading.value = false;
         }
-    }
+    };
+
+    // Função para verificar um código OTP enviado por SMS.
     const checkOTPPhone = async (otp) => {
         try {
-            loading.value = true
-            const response = await api.put(`/users/check-otp-phone/${otp}`)
-
-            return response
+            loading.value = true;
+            return await api.put(`/users/check-otp-phone/${otp}`);
         } catch (err) {
-            console.log(err.message)
-            throw err
+            console.log(err.message);
+            throw err;
         } finally {
             loading.value = false;
         }
-    }
+    };
+
+    // Função para verificar um código OTP para redefinição de senha.
     const checkOTPPassword = async (otp) => {
         try {
-            loading.value = true
-            const response = await api.get(`/users/check-otp-reset-password/${otp}`)
-
-            return response.data.otp
+            loading.value = true;
+            const response = await api.get(`/users/check-otp-reset-password/${otp}`);
+            return response.data.otp;
         } catch (err) {
-            console.log(err.message)
-            throw err
+            console.log(err.message);
+            throw err;
         } finally {
             loading.value = false;
         }
-    }
+    };
+
+    // Função para deslogar o usuário.
     const logout = async (session_id) => {
         try {
-            // Validação básica do session_id
             if (!session_id || typeof session_id !== "string" || session_id.trim() === "") {
                 throw new Error("ID de sessão inválido.");
             }
 
             loading.value = true;
 
-            // Chamar a API com timeout (exemplo com Axios)
             const response = await api.post(
                 "/users/destroy-session",
                 { session_id },
-                { timeout: 10000 } // 10 segundos de timeout
+                { timeout: 10000 }
             );
 
-            // Verificar se o logout foi bem-sucedido no backend
             if (response.status === 200) {
                 store.dispatch("setAccessToken", null);
-                clearRefreshToken(); // Assumindo que limpa localmente
+                clearRefreshToken();
             } else {
                 throw new Error("Falha ao encerrar a sessão no servidor.");
             }
         } catch (err) {
             console.error("Erro ao fazer logout:", err.message);
-            // Opcional: exibir notificação ao usuário
             alert("Erro ao encerrar a sessão. Tente novamente.");
-            // Não relançar o erro, a menos que necessário
         } finally {
             loading.value = false;
         }
     };
 
+    // Função para solicitar redefinição de senha.
     const forgotPassword = async (data) => {
         try {
-            loading.value = true
-            const response = await api.post("/users/forgot-password", data)
-            const user = response.data.user
-            return user
+            loading.value = true;
+            const response = await api.post("/users/forgot-password", data);
+            return response.data.user;
         } catch (err) {
-            console.log(err.message)
-            throw err
+            console.log(err.message);
+            throw err;
         } finally {
             loading.value = false;
         }
-    }
-
-    const updateUser = async (data) => {
-        try {
-            loading.value = true
-            await api.put("/users", data)
-        } catch (err) {
-            console.log(err.message)
-            throw err
-        } finally {
-            loading.value = false;
-        }
-    }
-
-    const resetPassword = async (data) => {
-        try {
-            loading.value = true
-            return await api.put(`/users/reset-password/${data.otp}`, {
-                new_password: data.new_password
-            })
-        } catch (err) {
-            console.log(err.message)
-            throw err
-        } finally {
-            loading.value = false;
-        }
-    }
-
-    // tem como finalidade remover a chave do refresh token dos cookies
-    function clearRefreshToken() {
-        Cookies.remove("session_id")
     };
 
-    // descodifica o token
+    // Função para atualizar os dados do usuário.
+    const updateUser = async (data) => {
+        try {
+            loading.value = true;
+            await api.put("/users", data);
+        } catch (err) {
+            console.log(err.message);
+            throw err;
+        } finally {
+            loading.value = false;
+        }
+    };
+
+    // Função para redefinir a senha do usuário.
+    const resetPassword = async (data) => {
+        try {
+            loading.value = true;
+            return await api.put(`/users/reset-password/${data.otp}`, {
+                new_password: data.new_password
+            });
+        } catch (err) {
+            console.log(err.message);
+            throw err;
+        } finally {
+            loading.value = false;
+        }
+    };
+
+    // Função para remover o token de sessão dos cookies.
+    function clearRefreshToken() {
+        Cookies.remove("session_id");
+    }
+
+    // Função para decodificar um token JWT.
     function decodeToken(token) {
         try {
             return jwtDecode(token);
@@ -219,24 +231,23 @@ export function useUsers() {
         }
     }
 
-    // Agenda a renovação automática do token
+    // Agenda a renovação do token automaticamente antes de expirar.
     function scheduleTokenRefresh(token) {
         if (refreshTimeout) clearTimeout(refreshTimeout);
 
         const decoded = decodeToken(token);
         if (!decoded || !decoded.exp) {
-            throw new Error("Token inválido ou sem campo exp.")
+            throw new Error("Token inválido ou sem campo exp.");
         }
 
-        // Calcula o tempo até a expiração (em ms)
         const expirationTime = decoded.exp * 1000;
         const currentTime = Date.now();
-        const timeUntilRefresh = expirationTime - currentTime - 60 * 1000; // 1 minuto antes de expirar
+        const timeUntilRefresh = expirationTime - currentTime - 60 * 1000;
 
         if (timeUntilRefresh > 0) {
             refreshTimeout = setTimeout(async () => {
                 try {
-                    const session_id = Cookies.get("session_id")
+                    const session_id = Cookies.get("session_id");
                     await refreshToken(session_id);
                 } catch (error) {
                     console.error("Erro ao renovar o token automaticamente:", error);
@@ -245,30 +256,14 @@ export function useUsers() {
         }
     }
 
+    // Define o token de sessão nos cookies.
     function setRefreshTokenFromCookies(token) {
-        const refreshToken = String(token)
-        const expirationDate = new Date();
-        expirationDate.setFullYear(expirationDate.getFullYear() + 1);
-        Cookies.set("session_id", refreshToken, {
+        Cookies.set("session_id", String(token), {
             secure: true,
             sameSite: "Strict",
-            expires: expirationDate
-        })
+            expires: new Date(new Date().setFullYear(new Date().getFullYear() + 1))
+        });
     }
 
-    return {
-        loading,
-        error,
-        register,
-        updateUser,
-        auth,
-        logout,
-        refreshToken,
-        checkEmail,
-        activeEmail,
-        checkOTPPhone,
-        checkOTPPassword,
-        forgotPassword,
-        resetPassword
-    }
+    return { loading, error, register, updateUser, auth, logout, refreshToken, checkEmail, activeEmail, checkOTPPhone, checkOTPPassword, forgotPassword, resetPassword };
 }
